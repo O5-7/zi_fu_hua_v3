3e5c2e75982e30ab4bebe24e1d27e5cf3130baec
517205163 <517205163@qq.com>
2023-02-13 22:59:00 +0800

    gray_block & gray_codes_no_reverse

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ code_source = code_source('xin_song_ti_16/code_sources_v3.txt', 'xin_song_ti_16/
 for x, y in code_source.code_dict.items():
     print(x, y.codes_num, y.codes)
 ndarrays_to_str = ndarrays_to_str(np.random.randint(0, 127, (10, 10)), code_source)
-print(random.Random().randint(-1,1))
+out = ndarrays_to_str.img_to_str(np.random.randint(0, 255, (10, 10), dtype=int), codes_style())
+print(out)
--- ndarrays_to_str.py
+++ ndarrays_to_str.py
@@ -3,16 +3,18 @@ import taichi as ti
 import random
 from code_source import code_source
 from codes_style import codes_style
+from code_source import one_color_codes
 
 ti.init(arch=ti.cuda)
 
 
 class ndarrays_to_str:
-    def __init__(self, media_ndarry: np.ndarray, code_source_obj: code_source):
+    def __init__(self, media_ndarray: np.ndarray, code_source_obj: code_source):
         self.codes_dict: dict = code_source_obj.code_dict
-        self.is_colorful = media_ndarry.shape[-1] == 3
-        shape_len = len(media_ndarry.shape)
-        if self.is_colorful: shape_len -= 1
+        self.is_colorful = media_ndarray.shape[-1] == 3
+        shape_len = len(media_ndarray.shape)
+        if self.is_colorful:
+            shape_len -= 1
         self.type = 'image' if shape_len == 2 else 'gif_or_video'
 
     # @ti.func
@@ -20,7 +22,9 @@ class ndarrays_to_str:
         out_str = ''
         for line in image:
             for pix in line:
-                pass
+                out_str += self.gray_block(pix)  # 替换转换方法
+            out_str += '[0m\n'
+        return out_str
 
     # @ti.func
     def gray_codes_reverse(self, pix: np.ndarray) -> str:
@@ -28,8 +32,40 @@ class ndarrays_to_str:
         if self.is_colorful:
             pix = int(np.average(pix))
         is_reverse = pix > 127
-        if is_reverse: pix -= 127
+        if is_reverse:
+            pix -= 127
+            out_str = '[38;2;0;0;0;48;2;255;255;255m'
+        else:
+            out_str = '[38;2;255;255;255;48;2;0;0;0m'
         while pix not in self.codes_dict.keys():
             pix += random.Random().randint(-1, 1)
-            if pix < 0: pix = 0
-            if pix > 127: pix = 127
+            if pix < 0:
+                pix = 0
+            if pix > 127:
+                pix = 127
+        find_strs: one_color_codes = self.codes_dict[pix]
+        out_str += find_strs.codes[random.Random().randint(0, find_strs.codes_num - 1)]
+        return out_str
+
+    def gray_block(self, pix: np.ndarray) -> str:
+        out_str = '　'
+        if self.is_colorful:
+            pix = int(np.average(pix))
+        pix = int(pix)
+        out_str = '[48;2;{0};{0};{0}m　'.format(pix)
+        return out_str
+
+    def gray_codes_no_reverse(self, pix: np.ndarray) -> str:
+        out_str = '[38;2;255;255;255;48;2;0;0;0m'
+        if self.is_colorful:
+            pix = int(np.average(pix))
+        pix = int(pix / 2)
+        while pix not in self.codes_dict.keys():
+            pix += random.Random().randint(-1, 1)
+            if pix < 0:
+                pix = 0
+            if pix > 127:
+                pix = 127
+        find_strs: one_color_codes = self.codes_dict[pix]
+        out_str += find_strs.codes[random.Random().randint(0, find_strs.codes_num - 1)]
+        return out_str
